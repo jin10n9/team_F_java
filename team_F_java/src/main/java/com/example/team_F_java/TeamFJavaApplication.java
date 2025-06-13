f79f208b015b0b31f7cb8a8cac523b9afda7795b
umaretate <sawasa4567@gmail.com>
2025-06-13 17:06:49 +0900

    spring boot

---
+++ team_F_java/src/main/java/com/example/team_F_java/TeamFJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.team_F_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamFJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamFJavaApplication.class, args);
+	}
+
+}
---
+++ team_F_java/src/test/java/com/example/team_F_java/TeamFJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.team_F_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamFJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
